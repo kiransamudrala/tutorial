98f7323197992c05407e7b8619e22bef1c29272d
Kiran Samudrala <67752748+kiransamudrala@users.noreply.github.com>
2020-07-24 16:09:07 -0400

    Update stream1.py

--- stream1.py
+++ stream1.py
@@ -1,1 +1,18 @@
-import streamlit as stimport numpy as npimport pandas as pdst.title('The Gungan Planet, Naboo!')user_input = ' 'user_input = st.text_input("What is youssa name?", ' ')if user_input==' ':    st.write('')else:    st.write('Messa welcomes youssa to our great planet Naboo, ',user_input, '!')from PIL import Imageimage = Image.open('/Users/kiran/Documents/my_py/jjb.jpg')st.image(image, caption='Messa Jar Jar Binks!',use_column_width=True)
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('The Gungan Planet, Naboo!')
+user_input = ' '
+
+user_input = st.text_input("What is youssa name?", ' ')
+
+
+if user_input==' ':
+    st.write('')
+else:
+    st.write('Messa welcomes youssa to our great planet Naboo, ',user_input, '!')
+
+from PIL import Image
+image = Image.open('jjb.jpg')
+st.image(image, caption='Messa Jar Jar Binks!',use_column_width=True)
