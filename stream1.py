efcb0537f2145fbfe38b8dfff95db5b4041477fd
kiransamudrala <samudralakiran7784@gmail.com>
2020-07-30 06:47:25 -0400

    places i travelled

--- stream1.py
+++ stream1.py
@@ -1,18 +1,61 @@
-import streamlit as st
-import numpy as np
+from geopy.extra.rate_limiter import RateLimiter
+from geopy.geocoders import Nominatim
 import pandas as pd
+import numpy as np 
+import streamlit as st
 
-st.title('The Gungan Planet, Naboo!')
-user_input = ' '
+locator = Nominatim(user_agent="myGeocoder")
+# location = locator.geocode('Smyrna,GA, USA')
+geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
 
-user_input = st.text_input("What is youssa name?", ' ')
+ads = ['ames ia','desmoines ia','chicago il','minneapolis mn','omaha ne','lincoln ne','new york city ny'
+       ,'las vegas nv','san francisco ca','san jose ca','columbus oh','fargo nd','niagara falls ny','tampa fl','savannah ga','raleigh nc'
+       ,'dallas tx','san juan pr','seattle wa','pensacola fl']
+df = pd.DataFrame(ads,columns=['ADDRESS'])
+df['location'] = df['ADDRESS'].apply(geocode)
+df['point'] = df['location'].apply(lambda loc: tuple(loc.point) if loc else None)
+df[['lat', 'lon', 'altitude']] = pd.DataFrame(df['point'].tolist(), index=df.index)
+
+# Adding code so we can have map default to the center of the data
+midpoint = (np.average(df['lat']), np.average(df['lon']))
+st.header('Cities I visited:')
+st.deck_gl_chart(
+            viewport={
+                'latitude': midpoint[0],
+                'longitude':  midpoint[1],
+                'zoom': 2.5
+            },
+            layers=[{
+                'type': 'ScatterplotLayer',
+                'data': df,
+                'radiusScale': 250,
+                'radiusMinPixels': 5,
+                'getFillColor': [248, 24, 148],
+            }]
+        )
 
 
-if user_input==' ':
-    st.write('')
-else:
-    st.write('Messa welcomes youssa to our great planet Naboo, ',user_input, '!')
+ads = ['India','USA','Australia','Spain','France','Italy']
+df = pd.DataFrame(ads,columns=['ADDRESS'])
+df['location'] = df['ADDRESS'].apply(geocode)
+df['point'] = df['location'].apply(lambda loc: tuple(loc.point) if loc else None)
+df[['lat', 'lon', 'altitude']] = pd.DataFrame(df['point'].tolist(), index=df.index)
 
-from PIL import Image
-image = Image.open('jjb.jpg')
-st.image(image, caption='Messa Jar Jar Binks!',use_column_width=True)
+# Adding code so we can have map default to the center of the data
+midpoint = (np.average(df['lat']), np.average(df['lon']))
+st.header('Countries I have been to:')
+st.deck_gl_chart(
+            viewport={
+                'latitude': midpoint[0],
+                'longitude':  midpoint[1],
+                'zoom': 0
+            },
+            layers=[{
+                'type': 'ScatterplotLayer',
+                'data': df,
+                'radiusScale': 250,
+                'radiusMinPixels': 5,
+                'getFillColor': [248, 24, 148],
+            }]
+        )
+
